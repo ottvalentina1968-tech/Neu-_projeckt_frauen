c7cf84a3f9fe15ecdf6883f59eb620fdd12c4595
ottvalentina1968-tech <ottvalentina1968@gmail.com>
2026-07-27 17:00:25 +0200

    доработка кода

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  // Нужно для GitHub Pages: https://ottvalentina1968-tech.github.io/Neu-_projeckt_frauen/
+  base: '/Neu-_projeckt_frauen/',
   plugins: [react()],
 })
